1904acebaf9017535e895eac5dca1eb167d06a0a
root <root@localhost.localdomain>
2024-07-01 09:14:26 -0500

    muestra de propiedades

--- src/app/components/properties/properties.component.ts
+++ src/app/components/properties/properties.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { GlobalService } from '../../services/global.service';
+import { CommonModule } from '@angular/common';
+import { virtualRouter } from '../../services/virtualRouter.service';
 
 @Component({
   selector: 'app-properties',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './properties.component.html',
   styleUrl: './properties.component.css'
 })
 export class PropertiesComponent {
 constructor (
-  public global: GlobalService
+  public global: GlobalService,
+  public virtualRouter: virtualRouter
 ) {}
 }
