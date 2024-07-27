329c076d0722e10f6a940f661e7e31c8383905af
root <root@localhost.localdomain>
2024-07-27 18:29:04 -0500

    inicializacion de pagina

--- src/app/components/property-detail/property-detail.component.ts
+++ src/app/components/property-detail/property-detail.component.ts
@@ -1,7 +1,7 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { AfterViewChecked, CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { GlobalService } from '../../services/global.service';
 import { virtualRouter } from '../../services/virtualRouter.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { SwiperEvents, } from 'swiper/types';
 import Swiper from 'swiper';
 @Component({
@@ -12,9 +12,17 @@ import Swiper from 'swiper';
   templateUrl: './property-detail.component.html',
   styleUrl: './property-detail.component.css'
 })
-export class PropertyDetailComponent {
+export class PropertyDetailComponent implements AfterViewChecked {
 constructor (
   public global: GlobalService,
-  public virtualRouter: virtualRouter
+  public virtualRouter: virtualRouter,
+  private viewportScroller: ViewportScroller
 ){}
+ngOnInit(): void {
+  window.scrollTo(0, 0); // Desplaza a la parte superior de la página
+}
+ngAfterViewChecked(): void {
+  window.scrollTo(0, 0);
+}
+
 }
